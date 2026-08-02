a5dbfa0af9a6c21abf1bad5e916d568b25b540c2
Jonatan Villalba <jvillalba@onikom.com.mx>
2026-08-02 12:17:19 -0300

    format app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,28 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+  withHttpTransferCacheOptions,
+} from '@angular/platform-browser';
 import { fakeHeroesBackendInterceptor } from './core/interceptors/fake-heroes-backend.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { API_ENDPOINTS } from './core/constants/api.constants';
+import { environment } from '../environments/environment';
+
+const heroesUrl = `${environment.apiBaseUrl}/${API_ENDPOINTS.heroes}`;
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(),
+    provideClientHydration(
+      withEventReplay(),
+      withHttpTransferCacheOptions({
+        filter: (req) => !req.url.startsWith(heroesUrl),
+      }),
+    ),
     provideHttpClient(withInterceptors([loadingInterceptor, fakeHeroesBackendInterceptor])),
   ],
 };
