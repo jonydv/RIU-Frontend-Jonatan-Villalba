aa5a3edd37ddab66764291a14186dc644a3295ab
Jonatan Villalba <jvillalba@onikom.com.mx>
2026-08-02 11:23:33 -0300

    creata a fake backend interceptor for heroes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { fakeHeroesBackendInterceptor } from './core/interceptors/fake-heroes-backend.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptors([fakeHeroesBackendInterceptor])),
   ],
 };
